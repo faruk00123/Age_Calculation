0bff116429b858172b18145de1850770a0fa4102
Md.Omar <omorfaruktamim01788@gmail.com>
2025-12-31 23:37:50 +0600

    first commit

--- app.js
+++ app.js
@@ -23,24 +23,18 @@ Btn.addEventListener("click", () => {
 
   if (m2 >= m1) {
     m3 = m2 - m1;
-  } else {
-    y3--;
-    m3 = 12 + m2 - m1;
   }
-  if (d2 >= d1) {
+  else if(m1 >= m2){
+    m3 = m1 - m2;
+  }
+
+  if(d2 >= d1){
     d3 = d2 - d1;
-  } else {
-    m3--;
-    d3 = getDaysInMonth(y1, m1) + d2 - d1;
   }
-  if (m3 < 0) {
-    m3 = 11;
-    y3--;
+  else if(d1 >= d2){
+    d3 = d1 - d2;
   }
   if (y3) {
     agecalculator.innerHTML = `👉 You are <span>${y3}</span> years, <span>${m3}</span> months, <span>${d3}</span> days old`;
   }
 });
-function getDaysInMonth(year, month) {
-  return new Date(year, month, 0).getDate();
-}
